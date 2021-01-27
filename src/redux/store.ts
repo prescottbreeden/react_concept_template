c66bcf2c6cbd3cae8748607f468c919f8a1f7bcc
prescottbreeden <prescott.breeden@gmail.com>
2021-01-27 01:40:11 -0800

    updated store organization

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -11,14 +11,22 @@ import { loaderReducer } from 'redux/reducers/core/loader.reducer';
 import { notificationsReducer } from 'redux/reducers/core/notifications.reducer';
 import { PERSON_KEY, LOADING_KEY, NOTIFICATION_KEY } from './keys';
 
-const rootReducer = combineReducers({
-  [PERSON_KEY]: personReducer,
+const coreReducers = {
   [LOADING_KEY]: loaderReducer,
   [NOTIFICATION_KEY]: notificationsReducer,
+};
+const featureReducers = {
+  [PERSON_KEY]: personReducer,
+};
+
+const rootReducer = combineReducers({
+  ...coreReducers,
+  ...featureReducers,
 });
 
 const featureMiddleware: any[] = [personMiddleware];
 
+// api -> normalize -> notification
 const coreMiddleware: any[] = [
   apiMiddleware,
   normalizeMiddleware,
